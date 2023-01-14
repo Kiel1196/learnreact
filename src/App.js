7d5f528a3ff6d210129583b2b8fbc9fab1183028
Kiel1196 <1kielleik1@gmail.com>
2023-01-14 09:09:01 +0700

    learn react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import TitleChanger from './Components/TitleChanger';
+import Counter from './Components/Counter';
+import './Css/Margin.css'
+import LearnInput from './Components/LearnInput';
+
+let MapelSatu = 'Belajar React !',
+    MapelDua = 'Counter',
+    MapelTiga = 'Belajar Input'
+  ;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TitleChanger className='MarginClass' data={MapelSatu} />
+      <Counter className='MarginClass' data={MapelDua} />
+      <LearnInput className='MarginClass' data={MapelTiga} />
     </div>
   );
 }
